fa46cfaf6c834b02fc698f002dffaea0b3cbcc17
Jorge Isaac <jorgeicruz2001@gmail.com>
2024-02-08 11:02:45 -0400

    Created the last playGame function that allows full interactive play and finished the project

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
     var randInt = Math.floor(randDecimal * 3);
 
     // To observe and track 
-    console.log(randInt);
+    // console.log(randInt);
 
     switch (randInt) {
         case 0:
@@ -26,22 +26,54 @@ function playRound(playerSelection, computerSelection) {
     let playerSelLower = playerSelection.toLowerCase();
 
     if ((playerSelLower == 'rock') && (computerSelection == 'Sizzors')) {
-        return "You Win! Rock beats Sizzors";
+        return ["You Win! Rock beats Sizzors", 1];
     }else if ((playerSelLower == 'rock') && (computerSelection == 'Paper')) {
-        return "You Lose! Paper beats Rock";
+        return ["You Lose! Paper beats Rock", 0];
     }else if ((playerSelLower == 'paper') && (computerSelection == 'Rock')) {
-        return "You Win! Papaer beats Rock";
+        return ["You Win! Papaer beats Rock", 1];
     }else if ((playerSelLower == 'paper') && (computerSelection == 'Sizzors')) {
-        return "You Lose! Sizzors beat Paper";
+        return ["You Lose! Sizzors beat Paper", 0];
     }else if ((playerSelLower == 'sizzors') && (computerSelection == 'Rock')) {
-        return "You Lose! Rock beats Sizzors";
+        return ["You Lose! Rock beats Sizzors", 0];
     }else if ((playerSelLower == 'sizzors') && (computerSelection == 'Paper')) {
-        return "You Win! Sizzors beat Paper";
+        return ["You Win! Sizzors beat Paper", 1];
     }
 
     else {
-        return "It's a draw!!";
+        return ["It's a draw!!", 2];
     }
 }
 
-console.log(playRound(prompt("Select: "), getComputerChoice()));
+function playGame() {
+
+    let winPlayer = 0;
+    let winComp = 0;
+
+    for (var i = 0; i < 5; i++) {
+        let playerSel = prompt("Type your selection (Rock, Paper, or Sizzors): ");
+
+        let playList = playRound(playerSel, getComputerChoice());
+
+        alert(playList[0]);
+
+        if (playList[1] == 0) {
+            winPlayer++;
+        }else if (playList[1] == 1) {
+            winComp++;
+        }
+    }
+
+    if (winPlayer>winComp) {
+        console.log("Player: " + winPlayer + " and Computer: " + winComp);
+        return "You Win!! Congrats!";
+    }else if (winComp>winPlayer) {
+        console.log("Player: " + winPlayer + " and Computer: " + winComp);
+        return "You Lose! Too Bad!";
+    }else {
+        console.log("Player: " + winPlayer + " and Computer: " + winComp);
+        return "It's a draw!";
+    }
+
+}
+
+console.log(playGame())
